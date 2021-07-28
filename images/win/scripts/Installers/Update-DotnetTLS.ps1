7a549e31f5c4f80c25322180ece80248bfe37574
Maxim Lobanov <maxim-lobanov@github.com>
2021-07-28 14:57:36 +0300

    Update Update-DotnetTLS.ps1

--- images/win/scripts/Installers/Update-DotnetTLS.ps1
+++ images/win/scripts/Installers/Update-DotnetTLS.ps1
@@ -3,17 +3,26 @@
 ##  Desc:  Update DotNetFramework security protocol to TLS 1.2
 ################################################################################
 
+Write-Host "Update-DotnetTLS.ps1"
+
 $registryPath = "HKLM:\SOFTWARE\Microsoft\.NETFramework\v4.0.30319"
 $name = "SchUseStrongCrypto"
 $value = "1"
-if(Test-Path $registryPath){
+if(Test-Path $registryPath) {
+    Write-Host "Into 1st condition"
     Set-ItemProperty -Path $registryPath -Name $name -Value $value -Type DWORD
 }
 
 $registryPath = "HKLM:\SOFTWARE\Wow6432Node\Microsoft\.NETFramework\v4.0.30319"
 if(Test-Path $registryPath){
+    Write-Host "Into 2nd condition"
     Set-ItemProperty -Path $registryPath -Name $name -Value $value -Type DWORD
 }
 
+Write-Host "Update-DotnetTLS.ps1 - tests"
+
 Invoke-PesterTests -TestFile "Tools" -TestName "DotnetTLS"
+
+
+Write-Host "Update-DotnetTLS.ps1 tests"
 
